c9dc61185ad909a467099237d0bd27188c027c37
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2009-08-06 08:20:40 +0000

    ported DB Classes to JDK5 varargs

--- gawky/trunk/src/gawky/database/DBN.java
+++ gawky/trunk/src/gawky/database/DBN.java
@@ -20,15 +20,7 @@ public class DBN
 		}
 	}
     
-    public static String getString(int pool, String sql) {
-    	try { 
-    		return DB.getString(pool, sql);
-    	} catch (Exception e) {
-    		return "";
-    	}
-    }
-    
-    public static String getString(int pool, String sql, Object[] params) {
+    public static String getString(int pool, String sql, Object... params) {
     	try { 
     		return DB.getString(pool, sql, params);
     	} catch (Exception e) {
@@ -36,15 +28,7 @@ public class DBN
     	}
 	}
 
-    public static String getString(Connection conn, String sql) {
-    	try { 
-    		return DB.getString(conn, sql);
-    	} catch (Exception e) {
-    		return "";
-    	}
-    }
-    
-    public static String getString(Connection conn, String sql, Object[] params) {
+    public static String getString(Connection conn, String sql, Object... params) {
     	try { 
     		return DB.getString(conn, sql, params);
     	} catch (Exception e) {
@@ -52,16 +36,7 @@ public class DBN
     	}
 	}
 
-	
-	public static int execute(String sql) {
-		try { 
-    		return DB.execute(sql);
-    	} catch (Exception e) {
-    		return 0;
-    	}
-	}
-	  
-	public static int execute(String sql, Object[] params) {
+	public static int execute(String sql, Object... params) {
 		try { 
     		return DB.execute(sql, params);
     	} catch (Exception e) {
@@ -69,16 +44,7 @@ public class DBN
     	}
 	}
 
-	public static int execute(int pool, String sql) 
-	{
-		try { 
-    		return DB.execute(pool, sql);
-    	} catch (Exception e) {
-    		return 0;
-    	}
-	}
-
-	public static int execute(int pool, String sql, Object[] params) {
+	public static int execute(int pool, String sql, Object... params) {
 		try { 
     		return DB.execute(pool, sql, params);
     	} catch (Exception e) {
@@ -86,7 +52,7 @@ public class DBN
     	}
 	}
 
-	public static int execute(Connection conn, String sql, Object[] params) {
+	public static int execute(Connection conn, String sql, Object... params) {
 		try { 
     		return DB.execute(conn, sql, params);
     	} catch (Exception e) {
@@ -99,7 +65,7 @@ public class DBN
      */
 
 
-	public static Map getRow(String sql, String[] params) {
+	public static Map getRow(String sql, Object... params) {
 		try { 
     		return DB.getRow(sql, params);
     	} catch (Exception e) {
@@ -107,7 +73,7 @@ public class DBN
     	}
 	}
 	
-	public static Map getRow(int pool, String sql, String[] params) {
+	public static Map getRow(int pool, String sql, Object... params) {
 		try { 
     		return DB.getRow(pool, sql, params);
     	} catch (Exception e) {
@@ -115,7 +81,7 @@ public class DBN
     	}
 	}
 
-	public static Map getRow(Connection conn, String sql, String[] params) {
+	public static Map getRow(Connection conn, String sql, Object... params) {
 		try { 
     		return DB.getHash(conn, sql, params);
     	} catch (Exception e) {
@@ -127,7 +93,7 @@ public class DBN
 	 * Eine ArrayList von Map
 	 */
 
-	public static ArrayList getRowList(String sql, String[] params) {
+	public static ArrayList getRowList(String sql, Object... params) {
 		try { 
     		return DB.getRowList(sql, params);
     	} catch (Exception e) {
@@ -135,7 +101,7 @@ public class DBN
     	}
 	}
 
-	public static ArrayList getRowList(int pool, String sql, String[] params) {
+	public static ArrayList getRowList(int pool, String sql, Object... params) {
 		try { 
     		return DB.getRowList(pool, sql, params);
     	} catch (Exception e) {
@@ -143,7 +109,7 @@ public class DBN
     	}
 	}
 
-	public static ArrayList getRowList(Connection conn, String sql, String[] params) {
+	public static ArrayList getRowList(Connection conn, String sql, Object... params) {
 		try { 
     		return DB.getRowList(conn, sql, params);
     	} catch (Exception e) {
@@ -151,7 +117,7 @@ public class DBN
     	}
 	}
 
-	public static HashMap getHash(String sql, String[] params) {
+	public static HashMap getHash(String sql, Object... params) {
 		try { 
     		return DB.getHash(sql, params);
     	} catch (Exception e) {
@@ -159,7 +125,7 @@ public class DBN
     	}
 	}
 	
-	public static HashMap getHash(int pool, String sql, String[] params) {
+	public static HashMap getHash(int pool, String sql, Object... params) {
 		try { 
     		return DB.getHash(pool, sql, params);
     	} catch (Exception e) {
@@ -167,7 +133,7 @@ public class DBN
     	}
 	}
 
-	public static HashMap getHash(Connection conn, String sql, String[] params) 
+	public static HashMap getHash(Connection conn, String sql, Object... params) 
 	{
 		try { 
     		return DB.getHash(conn, sql, params);
@@ -181,7 +147,7 @@ public class DBN
 	 *
 	 */
 
-	public static ArrayList getList(String sql, String[] params) 
+	public static ArrayList getList(String sql, Object... params) 
 	{
 		try { 
     		return DB.getList(sql, params);
@@ -190,7 +156,7 @@ public class DBN
     	}
 	}
 
-	public static ArrayList getList(int pool, String sql, String[] params) 
+	public static ArrayList getList(int pool, String sql, Object... params) 
 	{
 		try { 
     		return DB.getList(pool, sql, params);
@@ -199,7 +165,7 @@ public class DBN
     	}
 	}
 
-	public static ArrayList getList(Connection conn, String sql, String[] params) 
+	public static ArrayList getList(Connection conn, String sql, Object... params) 
 	{
 		try {
 			return DB.getList(sql, params);
