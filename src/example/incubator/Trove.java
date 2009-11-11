a73cc678da50956fea1c97177074f24f021915cc
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2009-11-11 21:54:07 +0000

    Trove Samples

---
+++ src/example/incubator/Trove.java
@@ -0,0 +1,32 @@
+package example.incubator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Trove {
+
+		public static void main(String[] args) {
+			
+			long start = System.currentTimeMillis();
+			
+			ArrayList sett_ = new ArrayList<String>(5000);
+			
+			for(int i=0; i < 6000; i++)
+				sett_.add(Integer.toString(i));
+
+			HashSet sett = new HashSet<String>(sett_);
+
+
+			HashSet sett2 = new HashSet<String>(5000);
+			
+			for(int i=0; i < 1000; i++)
+				sett2.add(Integer.toString(i));
+
+			System.out.println(System.currentTimeMillis() - start);
+			
+			sett_.retainAll(sett2);
+			
+			System.out.println(System.currentTimeMillis() - start);
+			
+		}
+}
