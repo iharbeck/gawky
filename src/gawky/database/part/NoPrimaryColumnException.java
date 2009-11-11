a35e1183cbff043a42cd08ceafdffc27a89345c0
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2009-11-11 21:51:58 +0000

    added Primary Exception

---
+++ src/gawky/database/part/NoPrimaryColumnException.java
@@ -0,0 +1,13 @@
+package gawky.database.part;
+
+import java.sql.SQLException;
+
+public class NoPrimaryColumnException extends SQLException 
+{
+	private static final long serialVersionUID = 1L;
+
+	public NoPrimaryColumnException(Table table)
+	{
+		super("No primary key defined for table [" + table.getTableName() + "]" );
+	}
+}
