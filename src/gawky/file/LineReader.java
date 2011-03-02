517e3fe10eebf8a9ce24f510416c4b8e5c6dc811
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2011-03-02 15:11:24 +0000

    cleanup LineReader code

--- src/gawky/file/LineReader.java
+++ src/gawky/file/LineReader.java
@@ -2,7 +2,6 @@ package gawky.file;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
@@ -10,16 +9,7 @@ public class LineReader {
 	
 	public static void processFile(String filename, LineHandler handler) throws Exception
 	{
-		BufferedReader is = new BufferedReader(new FileReader(filename));
-		String line = null;
-
-		try {   
-		    while((line = is.readLine()) != null)
-		    	handler.processLine(line);
-		} catch (CancelException e) {
-	    } finally {
-	    	is.close();
-	    }
+		processFile(filename, handler, "UTF-8");
 	}
 	
 	public static void processFile(String filename, LineHandler handler, String encoding) throws Exception
@@ -38,23 +28,6 @@ public class LineReader {
 	    	is.close();
 	    }
 	}
-	
-	public static void processUTF8File(String filename, LineHandler handler) throws Exception
-	{
-		Reader in = new InputStreamReader(new FileInputStream(filename), "UTF-8");
-		
-		BufferedReader is = new BufferedReader(in);
-		String line = null;
-		
-		try {   
-		    while((line = is.readLine()) != null)
-		    	handler.processLine(line);
-		} catch (CancelException e) {
-	    } finally {
-	    	is.close();
-	    }
-	}
-	
 	
 	public final static boolean startsWith(String[] records, String line) {
 		for(int i=0; i < records.length; i++)
