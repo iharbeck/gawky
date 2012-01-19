27f5a9b3b072468afcee6c5890737c1d2b0ba7d2
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2012-01-19 20:25:54 +0000

    Listprocessor

--- gawky/trunk/src/gawky/database/part/Table.java
+++ gawky/trunk/src/gawky/database/part/Table.java
@@ -769,7 +769,7 @@ public abstract class Table extends Part
 	}
 	
 	
-	public static <T extends Table> void find(Class<T> clazz, Connection conn, Result resulter, String where, Object[] params) throws Exception
+	public static <T extends Table> void find(Class<T> clazz, Connection conn, String where, Object[] params, Result resulter) throws Exception
 	{
 		T inst = clazz.newInstance();
 
