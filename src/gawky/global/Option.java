68190283925a6689b57ac7705e83b6150f1720af
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2012-06-16 21:15:32 +0000

    Logging Framework / Default log Folder

--- src/gawky/global/Option.java
+++ src/gawky/global/Option.java
@@ -8,6 +8,7 @@ import gawky.regex.Replacer;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -22,48 +23,49 @@ import org.apache.commons.configuration.tree.DefaultExpressionEngine;
 import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.spi.Configurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.RollingFileAppender;
 import org.apache.log4j.xml.DOMConfigurator;
 
-public class Option 
+public class Option
 {
 	private static Log log;
-	
+
 	static boolean initdone = false;
-	
-	private static String HELP    = "h";
-	
+
+	private static String HELP = "h";
+
 	static XMLConfiguration config = null;
 	private static String PROPERTY_FILE = "properties.xml";
-	
-	static CommandLine cmd     = null;
-	static Options     options = null;
+
+	static CommandLine cmd = null;
+	static Options options = null;
 
 	static Replacer replace = null;
-	
+
 	static String stage = null;
-	
-	private static String processalias(String alias) 
+
+	private static String processalias(String alias)
 	{
-		return replace.replaceFirst(alias, stage); 
+		return replace.replaceFirst(alias, stage);
 	}
-	
+
 	public static void main(String[] args) throws Exception
 	{
 		Option.init();
 		System.out.println(Option.processalias("db_${staging}"));
 	}
-	
+
 	/**
-	 * get Property from cmdline or configfile
-	 * cmdline overwrites
+	 * get Property from cmdline or configfile cmdline overwrites
 	 */
 	public static String getProperty(String alias, String def)
 	{
 		alias = processalias(alias);
-	
+
 		if(cmd != null && cmd.hasOption(alias))
 			return cmd.getOptionValue(alias);
 		else if(config != null)
@@ -71,13 +73,14 @@ public class Option
 		else
 			return def;
 	}
-	
-	
-	public static void enableXPath() {
+
+	public static void enableXPath()
+	{
 		config.setExpressionEngine(new XPathExpressionEngine());
 	}
-	
-	public static void disableXPath() {
+
+	public static void disableXPath()
+	{
 		config.setExpressionEngine(new DefaultExpressionEngine());
 	}
 
@@ -85,28 +88,27 @@ public class Option
 	{
 		return getProperty(alias, null);
 	}
-	
+
 	public static String[] getProperties(String alias)
 	{
 		alias = processalias(alias);
-		
+
 		if(cmd != null && cmd.hasOption(alias))
 			return cmd.getOptionValues(alias);
 		else if(config != null)
 			return config.getStringArray(alias);
-		
+
 		return new String[] {};
 	}
 
-	
 	public static boolean hasProperty(String alias)
 	{
 		alias = processalias(alias);
-		
+
 		return (cmd != null && cmd.hasOption(alias)) ||
-			   (config != null && (config.getString(alias) != null || "1".equals(config.getString(alias))));
+		        (config != null && (config.getString(alias) != null || "1".equals(config.getString(alias))));
 	}
-	
+
 	/**
 	 * Initialize Global properties with default properties file name "properties.xml"
 	 * 
@@ -116,224 +118,294 @@ public class Option
 	{
 		init(PROPERTY_FILE, "Properties", new String[0]);
 	}
-	
+
 	/**
 	 * reset init
+	 * 
 	 * @throws Exception
 	 */
-	public static void reset() 
+	public static void reset()
 	{
 		initdone = false;
 	}
-	
+
 	/**
 	 * Initialize Global properties with custom properties file name
 	 * 
-	 * @param propfile absolut or relative path to the configuration file
+	 * @param propfile
+	 *            absolut or relative path to the configuration file
 	 * @throws Exception
 	 */
 	public static void init(String propfile) throws Exception
 	{
 		init(propfile, "Properties", new String[0]);
 	}
-	
+
 	/**
 	 * Check if classname is available in current classpath
 	 * 
-	 * @param classname		name of class to find
-	 * @param info			comment send to option logger if class not available
+	 * @param classname
+	 *            name of class to find
+	 * @param info
+	 *            comment send to option logger if class not available
 	 * @return
 	 */
-	public final static boolean isClassInPath(String classname, String info) {
-		try {
+	public final static boolean isClassInPath(String classname, String info)
+	{
+		try
+		{
 			Class.forName(classname);
 			return true;
-		} catch (Exception e) {
+		}
+		catch(Exception e)
+		{
 			if(info != null)
 				log.info(info);
 			return false;
 		}
 	}
-	
-	public final static boolean isClassInClassloader(ClassLoader cl, String classname, String info) {
-		try {
+
+	public final static boolean isClassInClassloader(ClassLoader cl, String classname, String info)
+	{
+		try
+		{
 			cl.loadClass(classname);
 			return true;
-		} catch (Exception e) {
+		}
+		catch(Exception e)
+		{
 			if(info != null)
 				log.info(info);
 			return false;
 		}
 	}
-	
-	static void printHelp(String processname) {
+
+	static void printHelp(String processname)
+	{
 		// automatically generate the help statement
 		HelpFormatter formatter = new HelpFormatter();
-		formatter.printHelp( processname, options );
+		formatter.printHelp(processname, options);
 
 		System.exit(0);
 	}
-	
-	
+
+	static Logger root;
+	public static PrintStream createLoggingProxy(final PrintStream realPrintStream)
+	{
+		return new PrintStream(realPrintStream)
+		{
+			public void print(final String string)
+			{
+				root.info(string);
+				//realPrintStream.print(string);
+			}
+		};
+	}
+
 	/**
 	 * Initialze Global properties with custom properties file name
 	 * 
-	 * @param propfile 		absolut or relative path to the configuration file
-	 * @param processname	Optional name for the process
-	 * @param args			Command parameter for commandline parsing
+	 * @param propfile
+	 *            absolut or relative path to the configuration file
+	 * @param processname
+	 *            Optional name for the process
+	 * @param args
+	 *            Command parameter for commandline parsing
 	 * @throws Exception
 	 */
 	public static void init(String propfile, String processname, String args[]) throws Exception
 	{
 		if(initdone)
 			return;
-	
+
 		// Standard file encoding
 		System.setProperty("file.encoding", Constant.ENCODE_UTF8);
-		 
+
 		// Application BINROOT for LOG4J
 		System.setProperty("BINROOT", Locator.findBinROOT());
 
-		System.out.println("LOG4J File: " + Locator.findBinROOT() + "log4j.xml");
+		String filename = Locator.findBinROOT() + "log4j.xml";
+
+		System.out.println("LOG4J File: " + filename);
+
+		if(new File(filename).exists())
+			DOMConfigurator.configure(Locator.findBinROOT() + "log4j.xml");
+
+		root = Logger.getRootLogger();
 		
-		DOMConfigurator.configure(Locator.findBinROOT() + "log4j.xml");
-		
+		if(!root.getAllAppenders().hasMoreElements()) // No appenders means log4j is not initialized!
+		{
+			root.setLevel(Level.INFO);
+
+			String pattern = "%d{dd.MM.yyyy HH:mm:ss,SSS} %-5p [%t %c] - %m%n";
+
+			// ConsoleLogger
+			root.addAppender(new ConsoleAppender(new PatternLayout(pattern))); //PatternLayout.TTCC_CONVERSION_PATTERN;
+
+			// FileLogger
+			String logfolder = Locator.findBinROOT() + "../logs";
+			new File(logfolder).mkdir();
+
+			String logfile = logfolder + "/default.log";
+			root.addAppender(new RollingFileAppender(new PatternLayout(pattern), logfile)); //PatternLayout.TTCC_CONVERSION_PATTERN;
+
+			System.setOut(createLoggingProxy(System.out));
+			System.setErr(createLoggingProxy(System.err));
+		}
+
 		log = LogFactory.getLog(Option.class);
+
+		log.info("START");
 		log.info("BINROOT " + Locator.findBinROOT());
-	
 
 		// Properties parsen
-		try {
+		try
+		{
 			// XML Configfile
 			config = new XMLConfiguration();
 			config.setEncoding("UTF8");
 			config.setDelimiterParsingDisabled(true);
-			
+
 			if(Validation.notEmpty(propfile))
 			{
 				// Include von externen Configfiles: <import file="ALIAS" /> 
 				Grouper includer = new Grouper("<import file=\"(.*?)\".?/>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-	
+
 				String org = new String(Tool.readchararray(Locator.findBinROOT() + propfile));
-	
+
 				String[] includefile = includer.matchall(org);
-				
-				for(int i=0; i < includefile.length; i = i + 2)
+
+				for(int i = 0; i < includefile.length; i = i + 2)
 				{
-					try {
+					try
+					{
 						// read include file
-						String include = new String(Tool.readchararray(Locator.findBinROOT() + includefile[i+1]));
-					
+						String include = new String(Tool.readchararray(Locator.findBinROOT() + includefile[i + 1]));
+
 						org = org.replace(includefile[i], include.trim());
-					} catch (Exception e) {
+					}
+					catch(Exception e)
+					{
 						log.error("Configfile missing", e);
 					}
 				}
-			
+
 				config.load(new ByteArrayInputStream(org.trim().getBytes("UTF8")));
-				
+
 				stage = config.getString("staging", "test");
-				
+
 				replace = new Replacer("\\$\\{staging\\}");
-				
-				System.out.println("FOUND STAGE: ["  + stage + "]");
+
+				System.out.println("FOUND STAGE: [" + stage + "]");
 			}
-			
+
 			// Property Configfile
 			// config = new PropertiesConfiguration("TestServer.properties");
-		} catch(Exception e) {
+		}
+		catch(Exception e)
+		{
 			log.error(e);
 			throw e;
 		}
-		
-		boolean hascmd = isClassInPath("org.apache.commons.cli.CommandLineParser", 
-				                       "Commandline parser disabled Lib is missing!");
+
+		boolean hascmd = isClassInPath("org.apache.commons.cli.CommandLineParser",
+		        "Commandline parser disabled Lib is missing!");
 		if(hascmd && options != null)
 		{
 			// keine default options mehr
 			// initDefaultOptions(); 
-			
-			try {			
+
+			try
+			{
 				cmd = new PosixParser().parse(options, args);
 				//cmd = new GnuParser().parse(options, args);
 				//cmd = new BasicParser().parse(options, args);
-			} catch (Exception e) {
+			}
+			catch(Exception e)
+			{
 				System.out.println(e);
 				printHelp(processname);
 			}
-		
+
 			if(cmd.hasOption(HELP))
 				printHelp(processname);
 		}
-		
+
 		// DB initialisieren
-		
-		if(isClassInPath("gawky.database.DB", 
-						 "DB wurde nicht gefunden Parameter werden ignoriert!"))
+
+		if(isClassInPath("gawky.database.DB",
+		        "DB wurde nicht gefunden Parameter werden ignoriert!"))
 		{
 			DB.init();
 		}
-		
+
 		initdone = true;
-		
-		return; 
+
+		return;
 	}
-	
-	public static Options getOptions() {
+
+	public static Options getOptions()
+	{
 		if(options == null)
 			options = new Options();
-		
+
 		return options;
 	}
 
-	public static void addOption(String opt, boolean args, String description) {
+	public static void addOption(String opt, boolean args, String description)
+	{
 		addOption(opt, opt, args, description, false);
 	}
 
-	public static void addOption(String opt, boolean args, String description, boolean required) {
+	public static void addOption(String opt, boolean args, String description, boolean required)
+	{
 		addOption(opt, opt, args, description, required);
 	}
-	
-	public static void addOption(String opt, String optLong, boolean args, String description) {
+
+	public static void addOption(String opt, String optLong, boolean args, String description)
+	{
 		addOption(opt, optLong, args, description, false);
 	}
-	
-	public static void addOption(String opt, String optLong, boolean args, String description, boolean required) {
+
+	public static void addOption(String opt, String optLong, boolean args, String description, boolean required)
+	{
 		options = getOptions();
 
-		if(!options.hasOption(opt)) {
-			
+		if(!options.hasOption(opt))
+		{
+
 			org.apache.commons.cli.Option option = new org.apache.commons.cli.Option(opt, optLong, args, description);
 			option.setRequired(required);
-			
+
 			options.addOption(option);
 		}
 	}
-	
-	public static void initDefaultOptions() 
+
+	public static void initDefaultOptions()
 	{
 		// check if options already registered
 		// add default options (shorty, value, description)
 
-		addOption(HELP,    true,  "show Arguments");
+		addOption(HELP, true, "show Arguments");
 	}
-	
-	
-	 public static void addClasspath(String folder)
-	 {
-		 String fontpath = Locator.findBinROOT() + folder;
 
-		 try 
-		 {
+	public static void addClasspath(String folder)
+	{
+		String fontpath = Locator.findBinROOT() + folder;
+
+		try
+		{
 			URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
 			Class sysclass = URLClassLoader.class;
-			
-			Method method = sysclass.getDeclaredMethod("addURL", new Class[]{URL.class});
+
+			Method method = sysclass.getDeclaredMethod("addURL", new Class[] { URL.class });
 			method.setAccessible(true);
-			
+
 			File file = new File(fontpath);
-			method.invoke(sysloader, new Object[]{ file.toURI().toURL() });
-		 } catch (Exception e) {
-		 }
+			method.invoke(sysloader, new Object[] { file.toURI().toURL() });
+		}
+		catch(Exception e)
+		{
+		}
 	}
 }
