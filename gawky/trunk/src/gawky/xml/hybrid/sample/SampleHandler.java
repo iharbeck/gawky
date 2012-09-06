656208a99387afe0c93f2187f8bcffbf8ab6f06b
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2012-09-06 18:03:26 +0000

    Pullparser

--- gawky/trunk/src/gawky/xml/hybrid/sample/SampleHandler.java
+++ gawky/trunk/src/gawky/xml/hybrid/sample/SampleHandler.java
@@ -1,11 +1,8 @@
 package gawky.xml.hybrid.sample;
 
-import org.apache.xml.serialize.XMLSerializer;
-
 import gawky.file.Locator;
 import gawky.global.Constant;
 import gawky.xml.hybrid.XMLReaderPull;
-import gawky.xml.hybrid.alternative.XMLReaderStax;
 import gawky.xml.hybrid.sample.tag.TagFias2;
 import gawky.xml.hybrid.sample.tag.TagRecord;
 import gawky.xml.sax.SaxSample;
@@ -20,16 +17,14 @@ public class SampleHandler
 
 		XMLReaderPull reader = new XMLReaderPull();
 
-		for(int i = 0; i < 10; i++)
-		{
-			TagFias2 tagfias2 = new TagFias2();
-			TagRecord tagrecord = new TagRecord();
-			
-			reader.registerTagHandler(tagfias2);
-			reader.registerTagHandler(tagrecord);
+		TagFias2 tagfias2 = new TagFias2();
+		TagRecord tagrecord = new TagRecord();
 
-			reader.process(filename, Constant.ENCODE_UTF8);
-		}
+		reader.registerTagHandler(tagfias2);
+		reader.registerTagHandler(tagrecord);
+
+		reader.process(filename, Constant.ENCODE_UTF8);
+
 		System.out.println("ms " + (System.currentTimeMillis() - start));
 	}
 }
