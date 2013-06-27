7fd1113a641b4efd9ea532f22be02f7e880a927f
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2013-06-27 14:27:07 +0000

    SimpleSQLHolder

---
+++ gawky/trunk/src/main/java/gawky/database/SimpleSqlHolder.java
@@ -0,0 +1,137 @@
+package gawky.database;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * lable:
+ * 
+ * select * from client where name like ':name'
+ * 
+ * @author harb05
+ * 
+ */
+
+public class SimpleSqlHolder
+{
+	static HashMap<Object, HashMap<String, StringBuffer>> hsHolder = new HashMap<Object, HashMap<String, StringBuffer>>();
+
+	HashMap<String, StringBuffer> templates;
+
+	HashMap<String, String> map = null;
+	
+	public void addParameter(String key, String value)
+	{
+		if(map == null)
+			map = new HashMap<String, String>();
+		
+		map.put(key, value);
+	}
+	
+	public SimpleSqlHolder(Class<?> clazz)
+	{
+		templates = lookupTemplates(clazz);
+	}
+
+	public HashMap<String, StringBuffer> lookupTemplates(Class clazz)
+	{
+		HashMap<String, StringBuffer> templates = hsHolder.get(clazz);
+
+		if(templates == null)
+		{
+			templates = new HashMap<String, StringBuffer>();
+
+			try
+			{
+				String filename = clazz.getName();
+				filename = filename.substring(filename.lastIndexOf('.') + 1);
+				System.out.println(filename);
+				System.out.println(clazz.getCanonicalName());
+
+				String filepath = clazz.getResource(filename + ".sql").getPath();
+
+				BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF8"));
+
+				StringBuffer buf = null;
+
+				String line = null;
+				while((line = is.readLine()) != null)
+				{
+					String cline = line.trim();
+					if(cline.equals("") || cline.startsWith("#") || cline.startsWith("--"))
+						continue;
+
+					if(cline.matches("\\w*:"))
+					{
+						String id = line.replaceAll("[\\s:]", "");
+						
+						buf = new StringBuffer(5000);
+						templates.put(id, buf);
+						
+						System.out.println(">>" + id);
+						continue;
+					}
+					
+					if(buf != null)
+						buf.append(line).append("\n");
+				}
+				is.close();
+
+			}
+			catch(Exception e)
+			{
+			}
+
+			hsHolder.put(clazz, templates);
+		}
+
+		return templates;
+	}
+
+	public String getSql(String id) throws Exception
+	{
+		String content = templates.get(id).toString();
+
+		if(content == null)
+			throw new Exception("No SQL template available [" + id + "]");
+
+		if(map != null)
+			content = doParameter(content);
+
+		return content;
+	}
+
+	static Pattern patternParameter = Pattern.compile(":[a-zA-Z0-9\\._]+");
+
+	public String doParameter(String content)
+	{
+		StringBuffer buf = new StringBuffer(5000);
+
+		Matcher m = patternParameter.matcher(content);
+
+		int start = 0;
+		while(m.find())
+		{
+			String alias = content.substring(m.start() + 1, m.end());
+
+			buf.append(content.substring(start, m.start()));
+			String val = (String)map.get(alias);
+		
+			// if(val == null)
+			// val = "\n--:" + alias + "\n";
+			
+			if(val != null)
+				buf.append(val);
+
+			start = m.end();
+		}
+		buf.append(content.substring(start));
+
+		return buf.toString();
+	}
+}
