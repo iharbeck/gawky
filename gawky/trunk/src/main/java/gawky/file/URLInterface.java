544ee611f02e3153ad208ae4acf54c944d0b55bf
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2013-12-20 10:17:59 +0000

    FTP Handler

--- gawky/trunk/src/main/java/gawky/file/URLInterface.java
+++ gawky/trunk/src/main/java/gawky/file/URLInterface.java
@@ -2,5 +2,6 @@ package gawky.file;
 
 public interface URLInterface {
 	public void send(String url, String sourcepath) throws Exception;
-	public void retrieve(String url, String targetpath) throws Exception;
+	public String[] retrieve(String url, String targetpath) throws Exception;
+	public String[] retrieve(String url, String targetpath, boolean simulate) throws Exception;
 }
