d5a9880741d0629a5d468907269a788ae6330fe1
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2014-08-31 17:37:41 +0000

    CollectorList

---
+++ gawky/trunk/src/main/java/gawky/lang/list/CollectorList.java
@@ -0,0 +1,46 @@
+package gawky.lang.list;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class CollectorList<K, T>
+{
+	HashMap<K, ArrayList<T>> store;
+
+	int capacitylist;
+	
+	public CollectorList()
+	{
+		this(200, 50);
+	}
+	
+	public CollectorList(int capacity, int capacitylist)
+	{
+		store = new HashMap<K, ArrayList<T>>(capacity);
+		this.capacitylist = capacitylist;
+	}
+	
+	public void add(K key, T value)
+	{
+		ArrayList<T> list = store.get(key);
+
+		if(list == null)
+		{
+			list = new ArrayList<T>(capacitylist);
+			store.put(key, list);
+		}
+		
+		list.add(value);
+	}
+	
+	public ArrayList<T> get(K key)
+	{
+		ArrayList<T> poslist = store.get(key);
+		
+		if(poslist == null)
+			poslist = new ArrayList<T>();
+		
+		return poslist;
+	}
+}
+
