1756217d2e2e007a7f1fb826f42748249aceaad1
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2014-09-01 20:17:54 +0000

    cleanup

--- src/main/java/gawky/message/parser/XMLParser.java
+++ src/main/java/gawky/message/parser/XMLParser.java
@@ -7,10 +7,10 @@ import java.io.StringReader;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
-import org.jdom.xpath.XPath;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPath;
 
 /**
  * @author Ingo Harbeck
