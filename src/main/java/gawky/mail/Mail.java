c7a56d85d6b4b4799d039fad55cbb4e6cd96796a
Ingo Harbeck <ingo.harbeck@iharbeck.de>
2017-01-13 20:14:57 +0100

    set Mail sendDate explizit

--- src/main/java/gawky/mail/Mail.java
+++ src/main/java/gawky/mail/Mail.java
@@ -2,43 +2,24 @@ package gawky.mail;
 
 import gawky.global.Constant;
 import gawky.global.Option;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Properties;
-import java.util.zip.GZIPOutputStream;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Flags;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.*;
+import javax.mail.internet.*;
 import javax.mail.search.AndTerm;
 import javax.mail.search.FlagTerm;
 import javax.mail.search.SearchTerm;
 import javax.mail.search.SubjectTerm;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * @author Ingo Harbeck
@@ -245,6 +226,8 @@ public class Mail
 			}
 
 			message.setContent(multipart);
+
+			message.setSentDate(new Date());
 
 			// Send Mail
 			/*
